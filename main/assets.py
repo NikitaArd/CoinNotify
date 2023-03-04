126b9809d3e62c276b83e0ab9a1cd4e152218045
NikitaArd <11nik11255@gmail.com>
2023-03-04 12:20:41 +0100

    Corredcted the typos

--- main/assets.py
+++ main/assets.py
@@ -15,7 +15,7 @@ INTERVAL = 8  # interval in TIME_UNIT
 # Assets
 start_answer = """
 Witam!
-Jestem CryptoBot, pomogę ci sprawdzić kurs Bitcoin
+Jestem CryptoBot, pomogę ci sprawdzić kurs Bitcoin-a i Ethereum-a
 
 Jeśli masz pytania nie bój się pisać /help!
 """
@@ -24,15 +24,15 @@ help_answer = """
 Cześć!
 Widzę że potrzebujesz pomocy.
 Komendy:
-Wpisz /crypto aby sprawdić kurs Bitcoina
+Wpisz /crypto aby sprawdić kurs Bitcoin-a i Ethereum-a
 Wpisz /add aby zapisać się do Newsletter
 Wpisz /unadd aby wypisać się z Newsletter
 Informacja:
-Telegram Bot jest napisany na jęsyku programowania Python
+Telegram Bot jest napisany w języku programowania Python
 
 Twórca Nikita Smolenskyi
 GitHub : https://github.com/NikitaArd?tab=repositories
 """
 
-newsletter_add = "Pomyślnie zapisałeś się do newslettera."
+newsletter_add = "Pomyślnie zapisałeś się do listy mailingowej."
 newsletter_unadd = "Pomyślnie wypisałeś się z listy mailingowej."
